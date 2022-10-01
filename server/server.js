69acbb2cd1da9ea9d63a29147da17dea5a159785
AlinaTrukhina <46235932+AlinaTrukhina@users.noreply.github.com>
2022-09-30 20:57:22 -0500

    post calc inputs to array in server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const calculations = [];
 
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
@@ -8,6 +9,15 @@ app.use(bodyParser.urlencoded({extended:true}))
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
+// get data from onEquals() in client
+app.post('/calc', (req, res) => {
+  let newCalc = req.body;
+  console.log('posted calc object:', newCalc);
+
+  calculations.push(newCalc);
+  console.log('calc array is now:', calculations);
+  res.sendStatus(201);
+}); // end POST
 
 app.listen(3000, () => {
     console.log ('Server is running on port 3000');
