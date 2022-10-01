564029f304bb2996238acb5b55cb34a16e4e9be0
AlinaTrukhina <46235932+AlinaTrukhina@users.noreply.github.com>
2022-09-30 21:02:01 -0500

    add event listeners, add POST
    previous commits did not actually commit so this one is a "re-do"
    - add event listeners for operator buttons
    - add POST method to send input data to server

--- server/public/client.js
+++ server/public/client.js
@@ -1,16 +1,66 @@
 $(document).ready(onReady);
 
 let calculations = [];
+let operator = '';
 
 function onReady() {
 console.log('so ready');
 // event listeners
+
+    $('#plusBtn').on('click', selectPlus);
+    $('#minusBtn').on('click', selectMinus);
+    $('#multiplyBtn').on('click', selectMultiply);
+    $('#deleteBtn').on('click', selectDivide);
+
     $('#equalsBtn').on('click', onEquals);
+// render here or put it in a function below (GET?)
 //    render();
 }
 
-// POST input data to server
+selectPlus = () => operator = '+';
+selectMinus = () => operator = '-';
+selectMultiply = () => operator = '*';
+selectDivide = () => operator = '/';
 
+// POST input data to server
+function onEquals(evt) {
+    evt.preventDefault();
+
+    if ($('#inputA').val() == '' || $('#inputB').val() == 0 || 
+    operator == '') {
+        alert('please input values and click on operator')
+        return;
+    }
+
+    // create new object to send to server
+    newCalc = {
+        inputA : $('#inputA').val(), // number
+        inputB : $('#inputB').val(), // number
+        operator : operator,         // string
+        solution : ''                // string (will be converted to numer later)
+    }
+    console.log(newCalc);
+
+    $.ajax({
+        url: '/calc',
+        method: 'POST',
+        data: newCalc
+    })    
+
+    .then(response => {
+        console.log('POST calc inputs response', response);
+    })
+
+    .catch((err) => {
+        console.log('POST error', err);
+    });
+
+    $('#inputA').val('') // clear
+    $('#inputB').val('') // clear
+    operator = ''        // clear
+
+    getSolutions();
+}
 // GET new array with answers from server
 
 // DELETE data from server
