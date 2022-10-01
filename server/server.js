73251465d07fbbe407d674b96ef453461882ac01
AlinaTrukhina <46235932+AlinaTrukhina@users.noreply.github.com>
2022-09-30 22:06:16 -0500

    add switch function to find solution

--- server/server.js
+++ server/server.js
@@ -15,10 +15,58 @@ app.post('/calc', (req, res) => {
   console.log('posted calc object:', newCalc);
 
   calculations.push(newCalc);
-  console.log('calc array is now:', calculations);
+
   res.sendStatus(201);
 }); // end POST
+
+// send calc array to getSolutions() in client
+app.get('/calc', (req, res) => {
+
+  // get last item in array
+  let lastCalc = calculations[calculations.length-1];
+
+  // set the solution property to new value
+  lastCalc.solution = solver(lastCalc);
+
+  console.log('calc array is now:', calculations);
+  res.send(calculations); // sends array to client
+
+}); // end GET
 
 app.listen(3000, () => {
     console.log ('Server is running on port 3000');
   })
+
+  // functions
+  function solver(lastCalc) {
+    console.log('in solver:', lastCalc.operator);
+
+    // if (lastCalc.operator === '+') {
+    //   solution = Number(lastCalc.inputA) + Number(lastCalc.inputB);
+    // } 
+    // console.log('solution:', solution);
+    // return solution;
+    switch (lastCalc.operator) {
+  
+      case '+': 
+      console.log('in switch');
+      solution = Number(lastCalc.inputA) + Number(lastCalc.inputB);
+      console.log('solution', solution);
+      break;
+      case '-': 
+      solution = Number(lastCalc.inputA) - Number(lastCalc.inputB);
+      console.log('solution', solution);
+      break;
+      case '*': 
+      solution = Number(lastCalc.inputA) * Number(lastCalc.inputB);
+      console.log('solution', solution);
+      break;
+      case '/': 
+      solution = Number(lastCalc.inputA) / Number(lastCalc.inputB);
+      console.log('solution', solution);
+      break;
+      default: console.log('no match');
+    }
+    return solution;
+    //console.log('in solver', lastCalc.inputA + lastCalc.inputB);
+  }
